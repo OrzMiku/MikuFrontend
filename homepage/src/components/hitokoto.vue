766d00660a2e0ddf3318ac6cef61c024345fd87d
神奇のDz <miku@ecy.pink>
2023-09-08 11:37:07 +0800

    build: 依赖关系变动

--- homepage/src/components/hitokoto.vue
+++ homepage/src/components/hitokoto.vue
@@ -3,7 +3,7 @@
 </template>
 <script setup lang="ts">
 import { ref } from "vue";
-import getHitokoto from "@/utils/hitikoto";
+import getHitokoto from "@miku-utils/hitokoto";
 const params = {
   c: "d",
 };
