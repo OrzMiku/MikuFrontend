9dd9fbb62a1f6bde9ef2bd4db5d52e5ee10bbbac
神奇のDz <miku@ecy.pink>
2023-09-08 11:28:59 +0800

    style(packages:mikureq): 修改target为es6

--- packages/mikureq/src/main.d.ts
+++ packages/mikureq/src/main.d.ts
@@ -3,11 +3,11 @@ export type mikureqPrimise<T = any> = AxiosPromise<T>;
 export type mikureqRequestConfig<D = any> = AxiosRequestConfig<D>;
 export type mikureqInstance = AxiosInstance;
 declare class mikureq {
-  instance: mikureqInstance;
-  constructor(baseURL?: any, timeout?: number);
-  req<T = any>(config?: mikureqRequestConfig): mikureqPrimise<T>;
-  get<T = any>(config?: mikureqRequestConfig): mikureqPrimise<T>;
-  post<T = any>(config?: mikureqRequestConfig): mikureqPrimise<T>;
+    instance: mikureqInstance;
+    constructor(baseURL?: string, timeout?: number);
+    req<T = any>(config?: mikureqRequestConfig): mikureqPrimise<T>;
+    get<T = any>(config?: mikureqRequestConfig): mikureqPrimise<T>;
+    post<T = any>(config?: mikureqRequestConfig): mikureqPrimise<T>;
 }
 export default mikureq;
 //# sourceMappingURL=main.d.ts.map
--- packages/mikureq/src/main.js
+++ packages/mikureq/src/main.js
@@ -1,51 +1,29 @@
-"use strict";
-var __assign =
-  (this && this.__assign) ||
-  function () {
-    __assign =
-      Object.assign ||
-      function (t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-          s = arguments[i];
-          for (var p in s)
-            if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
-        }
-        return t;
-      };
-    return __assign.apply(this, arguments);
-  };
-Object.defineProperty(exports, "__esModule", { value: true });
-var axios_1 = require("axios");
-var mikureq = /** @class */ (function () {
-  function mikureq(baseURL, timeout) {
-    if (timeout === void 0) {
-      timeout = 10000;
-    }
-    this.instance = axios_1.default.create({
-      baseURL: baseURL,
-      timeout: timeout,
-    });
-  }
-  mikureq.prototype.req = function (config) {
-    var _this = this;
-    return new Promise(function (resolve, reject) {
-      _this.instance
-        .request(config)
-        .then(function (res) {
-          resolve(res);
-        })
-        .catch(function (err) {
-          reject(err);
+import axios from "axios";
+class mikureq {
+    constructor(baseURL, timeout = 10000) {
+        this.instance = axios.create({
+            baseURL,
+            timeout,
         });
-    });
-  };
-  mikureq.prototype.get = function (config) {
-    return this.req(__assign(__assign({}, config), { method: "get" }));
-  };
-  mikureq.prototype.post = function (config) {
-    return this.req(__assign(__assign({}, config), { method: "post" }));
-  };
-  return mikureq;
-})();
-exports.default = mikureq;
+    }
+    req(config) {
+        return new Promise((resolve, reject) => {
+            this.instance
+                .request(config)
+                .then((res) => {
+                resolve(res);
+            })
+                .catch((err) => {
+                reject(err);
+            });
+        });
+    }
+    get(config) {
+        return this.req(Object.assign(Object.assign({}, config), { method: "get" }));
+    }
+    post(config) {
+        return this.req(Object.assign(Object.assign({}, config), { method: "post" }));
+    }
+}
+export default mikureq;
 //# sourceMappingURL=main.js.map
